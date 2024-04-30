d7ad078e77c123af329ffd75bf2e474def56f9a2
Sufiyah Sajan <sufiyahsajan@gmail.com>
2024-04-30 17:36:37 +0200

    commit

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ sentry_sdk.init(
     dsn=os.getenv("SENTRY_DSN"),
     # enable_tracing=True,
     traces_sample_rate=1.0, # Set traces_sample_rate to 1.0 to capture 100% of transactions for performance monitoring.
-    profiles_sample_rate=1.0, # Set profiles_sample_rate to 1.0 to profile 100% of sampled transactions. We recommend adjusting this value in production
+    # profiles_sample_rate=1.0, # Set profiles_sample_rate to 1.0 to profile 100% of sampled transactions. We recommend adjusting this value in production
     integrations=[FlaskIntegration()],
     send_default_pii=True,
 )
